3614064978c5cc32aaaf9dcd212142dfa954ffa9
bluedesks3 <71720135+abramdarmaputra@users.noreply.github.com>
2022-10-20 19:30:59 +0700

    Update Data.js

--- Data.js
+++ Data.js
@@ -2,25 +2,25 @@ export const data = [
     {
       id: 1,
       nama: "Fatih Rizqy Hakim",
-      nim: "21120120140170",
+      nim: "211201201XXXXX",
       kelompok: "13"
     },
     {
       id: 2,
       nama: "Abram Darmaputra",
-      nim: "21120120130124",
+      nim: "211201201XXXXX",
       kelompok: "13"
     },
     {
       id: 3,
       nama: "Ahmad Aldani Herlangga",
-      nim: "21120120140168",
+      nim: "211201201XXXXX",
       kelompok: "13"
     },
     {
       id: 4,
       nama: "Audi Alvan Fikri",
-      nim: "21120120140095",
+      nim: "211201201XXXXX",
       kelompok: "13"
     }
   ];
